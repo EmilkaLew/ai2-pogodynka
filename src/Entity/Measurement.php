09baf0cf4ea9cb98d87a597f37a649ff8ba1ee88
Emilia Lewandowska <le53962>
2025-10-22 11:31:55 +0200

    labc

---
+++ src/Entity/Measurement.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MeasurementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MeasurementRepository::class)]
+class Measurement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'measurements')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Location $location = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $date = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: 0)]
+    private ?string $celsius = null;
+
+    #[ORM\Column]
+    private ?float $humidity = null;
+
+    #[ORM\Column]
+    private ?float $pressure = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 2)]
+    private ?string $precipitation = null;
+
+    #[ORM\Column]
+    private ?float $windSpeed = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLocation(): ?Location
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?Location $location): static
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTime
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTime $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getCelsius(): ?string
+    {
+        return $this->celsius;
+    }
+
+    public function setCelsius(string $celsius): static
+    {
+        $this->celsius = $celsius;
+
+        return $this;
+    }
+
+    public function getHumidity(): ?float
+    {
+        return $this->humidity;
+    }
+
+    public function setHumidity(float $humidity): static
+    {
+        $this->humidity = $humidity;
+
+        return $this;
+    }
+
+    public function getPressure(): ?float
+    {
+        return $this->pressure;
+    }
+
+    public function setPressure(float $pressure): static
+    {
+        $this->pressure = $pressure;
+
+        return $this;
+    }
+
+    public function getPrecipitation(): ?string
+    {
+        return $this->precipitation;
+    }
+
+    public function setPrecipitation(string $precipitation): static
+    {
+        $this->precipitation = $precipitation;
+
+        return $this;
+    }
+
+    public function getWindSpeed(): ?float
+    {
+        return $this->windSpeed;
+    }
+
+    public function setWindSpeed(float $windSpeed): static
+    {
+        $this->windSpeed = $windSpeed;
+
+        return $this;
+    }
+}
